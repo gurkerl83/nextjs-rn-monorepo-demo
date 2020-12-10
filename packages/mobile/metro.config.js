a3aa806c4c053068d88aacabc42e848494975324
gurkerl83 <gurkerl83@googlemail.com>
2020-12-10 10:34:27 +0100

    Initial commit

---
+++ packages/mobile/metro.config.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path')
+
+module.exports = {
+  watchFolders: [path.resolve(__dirname, '../..')],
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+}
